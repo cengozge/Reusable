f013b9076c8101738a35c7c4f7510ebc74a9f542
ozge.gokay <gokayozge@gmail.com>
2016-03-18 13:59:48 +0200

    --

---
+++ src/numericalOperations/NumericalOperations.java
@@ -0,0 +1,16 @@
+package numericalOperations;
+
+import java.util.List;
+
+import conversion.Conversions;
+
+public class NumericalOperations {
+
+	public static int findMaxOfArray(int[] A){
+		return findMaxOfList(Conversions.fromArrayToList(A));
+	}
+	
+	public static int findMaxOfList(List<Integer> list){
+		return list.stream().mapToInt(Integer::intValue).max().getAsInt();
+	}
+}
